6bc791ed6a0365e0442993349b1ed20ab69d88f1
Jorgen Nystad <jorgen@nystad.cc>
2020-03-22 18:48:12 +0100

    Logo

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,45 @@ import Waves from "./components/Waves";
 import MadeBy from "./components/MadeBy";
 import "./App.scss";
 
+function Circle({
+  r,
+  w = 1,
+  rotate = 0,
+  color
+}: {
+  r: number;
+  w?: number;
+  rotate?: number;
+  color: string;
+}) {
+  return (
+    <g transform={`rotate(${rotate + 180} 10 10)`}>
+      <path
+        d={`M 10,10 m -${r},0 A ${r} ${r} 0 1 1 10 ${10 + r}`}
+        stroke={color}
+        fill="none"
+        strokeWidth={w}
+      />
+    </g>
+  );
+}
+
+function Logo() {
+  return (
+    <div className="logo">
+      <svg viewBox="0 0 20 20" width={36} height={36}>
+        <Circle r={9} w={1} rotate={60} color="#046" />
+        <Circle r={7} w={0.9} rotate={-30} color="#09a" />
+        <Circle r={5} w={0.8} rotate={180} color="#aef" />
+        <Circle r={3} w={0.7} rotate={-45} color="#09a" />
+      </svg>{" "}
+      <h1 className="title">
+        <span className="pod">Pod</span>Crush
+      </h1>
+    </div>
+  );
+}
+
 const App: FC = () => {
   return (
     <BrowserRouter>
@@ -15,7 +54,7 @@ const App: FC = () => {
       <div className="App">
         <Player>
           <nav className="nav-bar">
-            <h1 className="title">PodCrush</h1>
+            <Logo />
             <Link to="/" className="icon">
               <svg width={16} height={16} viewBox="0 0 16 16">
                 <polygon
