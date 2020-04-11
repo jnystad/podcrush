77a07ac77e2a6efb2c6238a96cdca898ef4d68cf
Jorgen Nystad <jorgen@nystad.cc>
2020-04-11 18:30:16 +0200

    Add best podcasts endpoint

---
+++ api/best.ts
@@ -0,0 +1,17 @@
+import { NowRequest, NowResponse } from "@now/node";
+import request from "superagent";
+
+module.exports = (req: NowRequest, res: NowResponse) => {
+  request
+    .get(
+      "https://listen-api.listennotes.com/api/v2/best_podcasts?page=1&region=us&safe_mode=0"
+    )
+    .set("X-ListenAPI-Key", process.env.X_LISTENAPI_KEY)
+    .then((r) => {
+      res.removeHeader("Cache-Control");
+      res.setHeader("Cache-Control", "public, max-age=86400");
+      res.setHeader("Content-Type", "application/json");
+      res.send(r.text);
+    })
+    .catch(() => res.status(500));
+};
